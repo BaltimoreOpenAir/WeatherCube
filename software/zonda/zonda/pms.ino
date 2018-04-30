8138393a67310fdbaa0991e3fe32bec02ec9e34f
iamchriskelley <iamchriskelley@gmail.com>
2018-04-30 18:07:08 -0400

    minor updates

---
+++ software/zonda/zonda/pms.ino
@@ -0,0 +1,46 @@
+void read_pms_data()
+{
+  pms_note_code = 0;
+  int idx = PMS_DATA_INDEX;
+  if (pms.available()) {
+    char c0, c1, c2[2], cdata[30];
+    bool has_read = false;
+    c1 = pms.read();
+    for (int i = 0; i < 62; i++) { //more than one read frame
+      c0 = c1;
+      c1 = pms.read(); delay(10);
+      if (c0 == 0x42 && c1 == 0x4D) {
+        if(i > 0) pms_note_code = i;
+        pms.readBytes(cdata, 30); delay(10);
+        if (cdata[0] == 0x00 && cdata[1] == 0x1C) { //28-byte reading frame
+          has_read = true;
+          break;
+        }
+      }
+    }
+    if (has_read) {
+      //for(int j = 0; j < 3; j++){//could add second loop to cut byte array smaller, if needed
+        for (int i = 0; i < 30; i += 2) {
+          int i1 = cdata[i], i2 = cdata[i + 1];
+          if (i1 < 0) i1 = -i1 + 256;
+          if (i2 < 0) i2 = -i2 + 256;
+          int res = i1 * 256 + i2;
+          data_array[idx++] = res;
+        }
+      data_array[DATA_ARRAY_SIZE - 1] = pms_note_code;
+      pms.readBytes(c2, 2); delay(10);
+      pms.readBytes(c2, 2); delay(10);
+      pms.flush();
+    }
+  }
+}
+
+
+/*void reset_PMS(){
+  Serial.println("Restarting PM!");
+  digitalWrite(PMS_RESET, LOW);
+  delay(100);
+  digitalWrite(PMS_RESET, HIGH);
+  delay(4000);
+}*/
+
---
+++ software/zonda/zonda/rtc.ino
@@ -0,0 +1,61 @@
+void rtc_write_date(int sec, int mint, int hr24, int dotw, int dy, int mon, int yr)
+{
+  Wire.beginTransmission(RTC_ADDR);
+  Wire.write((uint8_t)TIME_REG);
+  Wire.write(dec2bcd(sec) | B10000000); //stops the oscillator (Bit 7, ST == 0)
+  Wire.write(dec2bcd(mint));
+  Wire.write(dec2bcd(hr24)); //hr
+  Wire.write(dec2bcd(dotw) | B00001000); //dotw
+  Wire.write(dec2bcd(dy)); //date
+  Wire.write(dec2bcd(mon)); //month
+  Wire.write(dec2bcd(yr)); //year
+
+  Wire.write((byte) 0);
+  //Wire.write((uint8_t)0x00);                     
+  /*
+     Wire.write(dec2bcd(05));
+    Wire.write(dec2bcd(04));                  //sets 24 hour format (Bit 6 == 0)
+    Wire.endTransmission();
+    Wire.beginTransmission(RTC_ADDR);
+    Wire.write((uint8_t)TIME_REG);
+    Wire.write(dec2bcd(30) | _BV(ST));    //set the seconds and start the oscillator (Bit 7, ST == 1)
+  */
+  Wire.endTransmission();
+}
+
+void rtc_read_timestamp(int mode)
+{
+  byte rtc_out[RTC_TS_BITS];
+  //int integer_time[RTC_TS_BITS];
+  Wire.beginTransmission(RTC_ADDR);
+  Wire.write((byte)0x00);
+  if (Wire.endTransmission() != 0) {
+    //Serial.println(F("no luck"));
+    //return false;
+  }
+  else {
+    //request 7 bytes (secs, min, hr, dow, date, mth, yr)
+    Wire.requestFrom(RTC_ADDR, RTC_TS_BITS);
+
+    // cycle through bytes and remove non-time data (eg, 12 hour or 24 hour bit)
+    for (int i = 0; i < RTC_TS_BITS; i++) {
+      rtc_out[i] = Wire.read();
+      //if (mode == 0) printBits(rtc_out[i]);
+      //else
+      //{
+        //byte b = rtc_out[i];
+        if (i == 0) rtc_out[i] &= B01111111;
+        else if (i == 3) rtc_out[i] &= B00000111;
+        else if (i == 5) rtc_out[i] &= B00011111;
+        //int j = bcd2dec(b);
+        //Serial.print(j);
+        //if(i < how_many - 1) Serial.print(",");
+      //}
+    }
+  }
+
+  for (int i = 0; i < RTC_TS_BITS ; i++) {
+    int ii = rtc_out[i];
+    integer_time[i] = bcd2dec(ii);
+  }
+}
